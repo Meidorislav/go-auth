186ce822a3d0de32eae3e435366ff5b628b1823a
Vladislav Bakin <meidorislav@gmail.com>
2026-04-18 23:09:53 +0500

    feat: register service

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -1,12 +1,35 @@
 package service
 
-import "github.com/alexedwards/argon2id"
+import (
+	"context"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/meidorislav/go-auth/internal/storage"
+)
+
+type Service struct {
+	DB *storage.Database
+}
+
+func InitService(db *storage.Database) *Service {
+	return &Service{
+		DB: db,
+	}
+}
 
 func HashPassword(password string) (string, error) {
 	return argon2id.CreateHash(password, argon2id.DefaultParams)
 }
 
-func CheckingHash(password, hash string) (bool, error) {	
-	return argon2id.ComparePasswordAndHash(password, hash) 
+func CheckingHash(password, hash string) (bool, error) {
+	return argon2id.ComparePasswordAndHash(password, hash)
 }
 
+func (s *Service) Register(ctx context.Context, login, password string) error {
+	hash, err := HashPassword(password)
+	if err != nil {
+		return err
+	}
+	s.DB.CreateUser(ctx, login, hash)
+	return nil
+}
