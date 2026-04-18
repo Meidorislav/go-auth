9dd84732ac28c63197babd1022bfe15522528fc0
Vladislav Bakin <meidorislav@gmail.com>
2026-04-18 19:30:47 +0500

    feat: database functions for `users`

---
+++ internal/storage/dto.go
@@ -0,0 +1,15 @@
+package storage
+
+import (
+	"time"
+
+	"github.com/gofrs/uuid/v5"
+)
+
+type User struct {
+	ID           uuid.UUID
+	Email        string
+	PasswordHash string
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
