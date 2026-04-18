8c8501621ab0de681d64c564ad2b4411f76a8c92
Vladislav Bakin <meidorislav@gmail.com>
2026-04-18 23:06:53 +0500

    feat: refresh generation

---
+++ pkg/token/refresh.go
@@ -0,0 +1,23 @@
+package token
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func GenerateRefreshToken() (raw, hash string, err error) {
+	b := make([]byte, 32)
+	if _, err = rand.Read(b); err != nil {
+		return "", "", err 
+	}
+	raw = hex.EncodeToString(b)
+	sum := sha256.Sum256([]byte(raw))
+	hash = hex.EncodeToString(sum[:])
+	return raw, hash, nil
+}
+
+func HashRefreshToken(raw string) string {
+	sum := sha256.Sum256([]byte(raw))
+	return hex.EncodeToString(sum[:])
+}
