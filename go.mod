2a867e1c942da046fc0181834550c4cafd716e2a
Vladislav Bakin <meidorislav@gmail.com>
2026-04-18 21:59:59 +0500

    feat: Run Migrations

--- go.mod
+++ go.mod
@@ -6,15 +6,17 @@ require (
 	github.com/alexedwards/argon2id v1.0.0
 	github.com/gofrs/uuid/v5 v5.4.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-migrate/migrate/v4 v4.19.1
 	github.com/jackc/pgx/v5 v5.9.1
 )
 
 require (
+	github.com/jackc/pgerrcode v0.0.0-20220416144525-469b46aa5efa // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/crypto v0.45.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.36.0 // indirect
 )
--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -2,10 +2,18 @@ package storage
 
 import (
 	"context"
+	"embed"
+	"errors"
 	"fmt"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+//go:embed migrations/*.sql
+var migrationsFS embed.FS
 
 type Database struct {
 	Pool *pgxpool.Pool
@@ -20,4 +28,19 @@ func InitDatabase(ctx context.Context, dsn string) (*Database, error) {
 		return nil, fmt.Errorf("unable to reach database: %w", err)
 	}
 	return &Database{Pool: pool}, nil
+}
+
+func RunMigrations(dsn string) error {
+	src, err := iofs.New(migrationsFS, "migrations")
+	if err != nil {
+		return fmt.Errorf("migrations source: %w", err)
+	}
+	m, err := migrate.NewWithSourceInstance("iofs", src, "pgx5://"+dsn)
+	if err != nil {
+		return fmt.Errorf("migrate init: %w", err)
+	}
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("migrate up: %w", err)
+	}
+	return nil
 }
